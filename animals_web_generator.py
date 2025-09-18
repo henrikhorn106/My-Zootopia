b734d6aa55f02d971abf5e97fcb8917418e6fbd4
Henrik Horn <henrik.horn106@gmail.com>
2025-09-18 23:06:31 +0200

    Added a filter functionality for skin type and improved the html output

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -23,22 +23,25 @@ def serialize_animal(animal_obj):
 
     output = ''
     output += '<li class="cards__item">'
-    output += f'<div class="card__title">{animal_obj['name']}</div>\n'
-    output += '<p class="card__text">'
-    output += f"<strong>Diet:</strong> {animal_obj['characteristics']['diet']}<br/>\n"
-    output += f"<strong>Location:</strong> {animal_obj['locations'][0]}<br/>\n"
+    output += (f'<div class="card__title">{animal_obj['name']} '
+               f'({animal_obj['taxonomy']['scientific_name']})</div>\n')
+    if 'slogan' in animal_obj['characteristics']:
+        output += f'<p class="card__text"><em>{animal_obj['characteristics']['slogan']}</em></p>'
+    output += '<div class="card__text">'
+    output += '<ul>'
+    output += f"<li><strong>Diet:</strong> {animal_obj['characteristics']['diet']}</li>\n"
+    output += f"<li><strong>Location:</strong> {animal_obj['locations'][0]}</li>\n"
     if 'type' in animal_obj['characteristics']:
-        output += f"<strong>Type:</strong> {animal_obj['characteristics']['type']}<br/>\n"
-    output += "</p>"
+        output += f"<li><strong>Type:</strong> {animal_obj['characteristics']['type']}</li>\n"
+    output += '</ul>'
+    output += "</div>"
     output += "</li>"
 
     return output
 
 
-def create_animals_html():
+def create_animals_html(animals_data):
     """Fetches and formats animal data into HTML list item strings."""
-
-    animals_data = load_data('animals_data.json')
 
     output = ""
     for animal in animals_data:
@@ -47,17 +50,60 @@ def create_animals_html():
     return output
 
 
-def generate_html():
+def generate_html(data):
     """Generates an HTML file by replacing placeholders in a template with formatted animal data."""
 
     with open('animals_template.html', 'r', encoding="utf-8") as file:
         template = file.read()
 
-    template = template.replace("__REPLACE_ANIMALS_INFO__", create_animals_html())
+    template = template.replace("__REPLACE_ANIMALS_INFO__", create_animals_html(data))
 
     with open('animals.html', 'w', encoding="utf-8") as file:
         file.write(template)
 
 
+def filter_data(data, skin_type):
+    """Filters data by skin type."""
+
+    data = [animal for animal in data if animal['characteristics']['skin_type'] == skin_type]
+    return data
+
+
+def get_skin_types(data):
+    """Returns a set of all available skin types."""
+
+    skin_types = set()
+    for animal in data:
+        skin_types.add(animal['characteristics']['skin_type'])
+
+    sorted_skin_types = sorted(list(skin_types))
+    return sorted_skin_types
+
+
+def main():
+    """Main function."""
+
+    # Get skin type
+    data = load_data("animals_data.json")
+    skin_types = get_skin_types(data)
+
+    print("The following skin types are available:")
+    for skin_type in skin_types:
+        print(f"- {skin_type}")
+
+    # Filter for skin type
+    while True:
+        skin_type_input = input("\nSelect a skin type you want to filter for: ").strip()
+
+        if skin_type_input in skin_types:
+            break
+
+        print("Invalid skin type. Please try again.")
+
+    filtered_data = filter_data(data, skin_type_input)
+
+    generate_html(filtered_data)
+
+
 if __name__ == "__main__":
-    generate_html()
+    main()
